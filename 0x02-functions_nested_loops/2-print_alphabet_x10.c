14b04d7a9598407f620e593f069b34deecb0e11e
isaaco24 <isaacadegboyega24@gmail.com>
2023-02-21 16:36:27 -0800

    isalpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,16 +11,15 @@ void print_alphabet_x10(void)
 	int i;
 
 	i = 0;
-
 	while (i < 10)
-	{
+		{	
 		ch = 'a'
 		while (ch <= "z")
 		{
 		 	_putchar(ch);
 			ch++
 		}
-		_putchar('\n');
-		i++;
-	}
+			_putchar('\n');
+			i++;
+		}
 }
