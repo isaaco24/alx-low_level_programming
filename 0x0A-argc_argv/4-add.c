03b32969706e1fd1a6d1b0b1a3d55355cd32402e
isaaco24 <isaacadegboyega24@gmail.com>
2023-03-10 15:46:51 -0800

    3-mul.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -27,6 +27,7 @@ int check_num(char *str)
 	}
 	return (1);
 }
+
 /**
  * main - Print the name of the program
  * @argc: Count arguments
@@ -41,15 +42,21 @@ int main(int argc, char *argv[])
 	/*Declaring variables*/
 
 	int count;
+
 	int str_to_int;
+
 	int sum = 0;
+
 	count = 1;
+
 	while (count < argc) /*Goes through the whole array*/
 	{
 		if (check_num(argv[count]))
 		{
 			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+
 			sum += str_to_int;
+
 		}
 		/*Condition if one of the number contains symbols that are not digits*/
 		else
