3814cdd30d7632ce94688fe43814421e4fea309c
isaaco24 <isaacadegboyega24@gmail.com>
2023-08-21 20:16:27 -0700

    0x18dynamics

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+float divide(int a, int b) {
+    if (b != 0) {
+        return (float)a / b;
+    } else {
+        printf("Error: Division by zero!\n");
+        return 0.0;
+    }
+}
+
+int main() {
+    // Your main function code here...
+    return 0;
+}
